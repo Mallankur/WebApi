74903b91bf1b3a0fb988ea622a8d387ad950924e
ankur <ankur7256@gmail.com>
2022-10-03 18:00:31 +0530

    ?

---
+++ Basic of Asp .Net core/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_of_Asp.Net_core
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            var demomethod = DemoMethodAsync("Task1");
+            Console.WriteLine("cursore Moved to next Line in Main () without waiting forr DemoMethodAsync() compleection");
+            var longruningmethod = LongRunningOperationAsync("Task2");
+            Console.WriteLine("Now waiting for task to finished");
+            Task.WaitAll(longruningmethod,demomethod); // now waiting 
+            Console.WriteLine("Exiting Commandline");
+            Console.ReadLine(); 
+        }
+
+        private static async Task<string>   LongRunningOperationAsync(string  taskName)
+        {
+            Console.WriteLine( taskName);
+            await Task.Delay(5000);
+            Console.WriteLine($"longrunning oprstion for {taskName}");
+            return "Hello there from long operations";
+
+            
+        }
+
+        private static async Task DemoMethodAsync(string  taskname)
+        {
+            Task<string> longrunningTask = LongRunningOperationAsync(taskname);
+            Console.WriteLine("work now excute independly in demo Methodasync()");
+            var result = await longrunningTask;
+            Console.WriteLine(result);
+        }
+      
+    }
+}
---
+++ learning web api/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace learning_web_api
+{
+    public class Book
+    {
+        public string? Id { get; set; } = null!;
+        public decimal Price  { get; set; }
+        public string Category { get; set; } = null!;
+        public string Author { get; set; } = null!; 
+
+    }
+}
